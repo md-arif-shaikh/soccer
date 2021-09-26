0c01090840005b584e0b419cbe653abd22c5164d
md-arif-shaikh <arifshaikh.astro@gmail.com>
2021-09-26 22:35:58 +0530

    refactored and added functions, README added

--- soccer.el
+++ soccer.el
@@ -33,55 +33,150 @@
   (let* ((url (cdr (assoc club (cdr (assoc league soccer--leagues))))))
     url))
 
-(defun soccer--get-league-data (league club data-type)
+(defun soccer--get-league-data (league club data-type num-of-results)
+  "Get NUM-OF-RESULT number of DATA-TYPE for a CLUB of a LEAGUE."
+  (let* ((league-data (soccer--get-league-data-alist league club data-type))
+	 (dates (cdr (assoc "date" league-data)))
+	 (times (cdr (assoc "time" league-data)))
+	 (homes (cdr (assoc "home" league-data)))
+	 (awayes (cdr (assoc "away" league-data)))
+	 results
+	 msg-str
+	 (number-of-results (length dates)))
+    (when (string-equal data-type "results")
+      (setq results (cdr (assoc "result" league-data))))
+    (if num-of-results
+	(setq num-of-results (min number-of-results num-of-results))
+      (setq num-of-results number-of-results))
+    (setq msg-str (cl-loop for n from 0 to (1- num-of-results)
+			   collect (let* ((date (nth n dates))
+					  (time (nth n times))
+					  (home (nth n homes))
+					  (away (nth n awayes))
+					  result)
+				     (when (string-equal data-type "results")
+				       (setq result (nth n results)))
+				     (format "%s %s:  %s vs %s %s" date time home away (if (string-equal data-type "results") (format "%s" result) "")))))
+    (message "%s" (string-join msg-str "\n"))))
+
+(defun soccer--get-league-data-in-org (league club data-type num-of-results)
+  "Get NUM-OF-RESULT number of DATA-TYPE for a CLUB of a LEAGUE."
+  (let* ((league-data (soccer--get-league-data-alist league club data-type))
+	 (dates (cdr (assoc "date" league-data)))
+	 (times (cdr (assoc "time" league-data)))
+	 (homes (cdr (assoc "home" league-data)))
+	 (awayes (cdr (assoc "away" league-data)))
+	 results
+	 (file-name (format "/tmp/soccer_%s_%s.org" club data-type))
+	 (number-of-results (length dates)))
+    (when (string-equal data-type "results")
+      (setq results (cdr (assoc "result" league-data))))
+    (if num-of-results
+	(setq num-of-results (min number-of-results num-of-results))
+      (setq num-of-results number-of-results))
+    (with-temp-buffer
+      (insert (format "* %s for %s\n\n" data-type club))
+      (insert "|--|--|--|--|--|\n")
+      (insert (format "|Date |Time | Home | Away | %s \n" (if (string-equal data-type "results") "score |" "")))
+      (insert "|--|--|--|--|\n")
+      (dolist (n (number-sequence 0 (1- num-of-results)))
+	(let* ((date (nth n dates))
+	       (time (nth n times))
+	       (home (nth n homes))
+	       (away (nth n awayes))
+	       result)
+	  (when (string-equal data-type "results")
+	    (setq result (nth n results)))
+	  (insert (format "|%s|%s|%s|%s|%s\n" date time home away (if (string-equal data-type "results") (format "%s|" result) "")))
+	  (insert "|--|--|--|--|\n")))
+      (forward-line (* -1 num-of-results))
+      (org-table-align)
+      (write-region (point-min) (point-max) file-name)
+      (find-file file-name))))
+
+(defun soccer--get-league-data-alist (league club data-type)
   "Get data for a CLUB of a LEAGUE."
   (let* ((url (concat (soccer--get-league-url league club) (format "/%s/" data-type))))
     (with-current-buffer (url-retrieve-synchronously url)
       (let* ((dom (libxml-parse-html-region (point-min) (point-max)))
-	     (dates (dom-by-class dom "kick_t_dt"))
-	     (times (dom-by-class dom "kick_t_ko"))
-	     (homes (dom-by-class dom "home_o"))
-	     (awayes (dom-by-class dom "away_o"))
-	     results
+	     (dates-dom (dom-by-class dom "kick_t_dt"))
+	     (times-dom (dom-by-class dom "kick_t_ko"))
+	     (homes-dom (dom-by-class dom "home_o"))
+	     (awayes-dom (dom-by-class dom "away_o"))
+	     results-dom
 	     (file-name (format "/tmp/soccer_%s_%s.org" club data-type))
-	     (number-of-results (length dates)))
+	     (number-of-results (length dates-dom))
+	     dates
+	     times
+	     homes
+	     awayes
+	     results)
 	(when (string-equal data-type "results")
-	  (setq results (dom-by-class dom "score cshas_ended")))
-	(with-temp-buffer
-	  (insert (format "* %s for %s\n\n" data-type club))
-	  (insert "|--|--|--|--|--|\n")
-	  (insert (format "|Date |Time | Home | Away | %s \n" (if (string-equal data-type "results") "score |" "")))
-	  (insert "|--|--|--|--|\n")
-	  (dolist (n (number-sequence 0 (1- number-of-results)))
-	    (let* ((date (dom-texts (nth n dates)))
-		   (time (dom-texts (nth n times)))
-		   (home (dom-texts (nth n homes)))
-		   (away (dom-texts (nth n awayes)))
-		   result)
-	      (when (string-equal data-type "results")
-		(setq result (dom-texts (nth n results))))
-	      (insert (format "|%s|%s|%s|%s|%s\n" date time home away (if (string-equal data-type "results") (format "%s|" result) "")))
-	      (insert "|--|--|--|--|\n")))
-	  (forward-line (* -1 number-of-results))
-	  (org-table-align)
-	  (write-region (point-min) (point-max) file-name)
-	  (find-file file-name))))))
+	  (setq results-dom (dom-by-class dom "score cshas_ended"))
+	  (setq results (cl-loop for n from 0 to number-of-results
+			       collect (dom-texts (nth n results-dom)))))
+	(setq dates (cl-loop for n from 0 to number-of-results
+			     collect (dom-texts (nth n dates-dom))))
+	(setq times (cl-loop for n from 0 to number-of-results
+			     collect (dom-texts (nth n times-dom))))
+	(setq homes (cl-loop for n from 0 to number-of-results
+			     collect (dom-texts (nth n homes-dom))))
+	(setq awayes (cl-loop for n from 0 to number-of-results
+			      collect (dom-texts (nth n awayes-dom))))
+	`(("date" . ,dates)
+	  ("time" . ,times)
+	  ("home" . ,homes)
+	  ("away" . ,awayes)
+	  ("result" . ,results))))))
 
-(defun soccer--fixtures (league club)
+(defun soccer--fixtures-full-in-org (league club)
   "Fixtures of CLUB of LEAGUE."
   (interactive
    (let* ((league-name (completing-read "league: " soccer--league-names))
 	  (club-name (completing-read "club: " (mapcar 'car (cdr (assoc league-name soccer--leagues))))))
      (list league-name club-name)))
-  (soccer--get-league-data league club "fixtures"))
+  (soccer--get-league-data-in-org league club "fixtures" nil))
 
-(defun soccer--results (league club)
+(defun soccer--fixtures-next (league club)
   "Fixtures of CLUB of LEAGUE."
   (interactive
    (let* ((league-name (completing-read "league: " soccer--league-names))
 	  (club-name (completing-read "club: " (mapcar 'car (cdr (assoc league-name soccer--leagues))))))
      (list league-name club-name)))
-  (soccer--get-league-data league club "results"))
+  (soccer--get-league-data league club "fixtures" 1))
+
+(defun soccer--fixtures-next-5 (league club)
+  "Fixtures of CLUB of LEAGUE."
+  (interactive
+   (let* ((league-name (completing-read "league: " soccer--league-names))
+	  (club-name (completing-read "club: " (mapcar 'car (cdr (assoc league-name soccer--leagues))))))
+     (list league-name club-name)))
+  (soccer--get-league-data league club "fixtures" 5))
+
+
+(defun soccer--results-full-in-org (league club)
+  "Fixtures of CLUB of LEAGUE."
+  (interactive
+   (let* ((league-name (completing-read "league: " soccer--league-names))
+	  (club-name (completing-read "club: " (mapcar 'car (cdr (assoc league-name soccer--leagues))))))
+     (list league-name club-name)))
+  (soccer--get-league-data-in-org league club "results" nil))
+
+(defun soccer--results-last (league club)
+  "Fixtures of CLUB of LEAGUE."
+  (interactive
+   (let* ((league-name (completing-read "league: " soccer--league-names))
+	  (club-name (completing-read "club: " (mapcar 'car (cdr (assoc league-name soccer--leagues))))))
+     (list league-name club-name)))
+  (soccer--get-league-data league club "results" 1))
+
+(defun soccer--results-last-5 (league club)
+  "Fixtures of CLUB of LEAGUE."
+  (interactive
+   (let* ((league-name (completing-read "league: " soccer--league-names))
+	  (club-name (completing-read "club: " (mapcar 'car (cdr (assoc league-name soccer--leagues))))))
+     (list league-name club-name)))
+  (soccer--get-league-data league club "results" 5))
 
 (provide 'soccer)
 ;;; soccer.el ends here
