fc966680e2c46d40a4b30e11d296e517ddb54042
md-arif-shaikh <arifshaikh.astro@gmail.com>
2021-09-29 16:54:52 +0530

    imporved commentary

--- soccer.el
+++ soccer.el
@@ -23,8 +23,34 @@
 ;;; Commentary:
 ;;
 ;; This package brings soccer (football) fixtures, results in your Emacs.
+;; Currently it works for
+;;
+;; 1. Premier League (England)
+;; 2. La Liga (Spain)
+;;
+;; Other leagues could be easily included and would be in future.
+;;
+;; To get the time of kick off in your local time, you may want to set the
+;; following values accordingly
+;;
+;; (setq soccer--source-time-utc-offfset "+0200") ;; this is not needed to change in most cases
+;; (setq soccer--local-time-utc-offset "+0530") ;; this should be changed to your local one
+;;
+;; Common Functions:
+;;
+;; This package comes with handy interactive functions to get useful information in your minibuffer.
+;; To invoke a function use "M-x Function" where Function could be any of the following functions
+;;
+;; Functions	                Actions
+;; soccer--fixtures-next	Fixture for the Next match
+;; soccer--fixtures-next-5	Fixtures of the Next 5 matches
+;; soccer--fixtures-full-in-org	Full fixtures saved in org file
+;; soccer--results-last	        Result of the last match
+;; soccer--results-last-5	Results of the last 5 matches
+;; soccer--results-full-in-org	Full list of results in org file
 
 ;;; Code:
+
 (require 'soccer-leagues)
 (require 'soccer-time-utils)
 (require 'org)
