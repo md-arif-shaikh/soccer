72cc0e5f80892d3d7f12bd894f5a614cd5ce437d
md-arif-shaikh <arifshaikh.astro@gmail.com>
2021-10-04 10:10:46 +0530

    added dash as package requirement

--- soccer.el
+++ soccer.el
@@ -4,7 +4,7 @@
 
 ;; Author: Md Arif Shaikh <arifshaikh.astro@gmail.com>
 ;; Homepage: https://github.com/md-arif-shaikh/soccer
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "25.1") (dash "2.19.1"))
 ;; Version: 0.1.1
 ;; Keywords: games
 
