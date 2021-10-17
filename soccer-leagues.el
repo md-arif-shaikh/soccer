6666d50261a7c97d5255b2cc5ea9e9e0c0cfc858
md-arif-shaikh <arifshaikh.astro@gmail.com>
2021-10-17 13:14:16 +0530

    add/remove new league easily

--- soccer-leagues.el
+++ soccer-leagues.el
@@ -43,28 +43,36 @@
 				collect (cons (car (dom-strings (dom-by-tag d 'a))) (concat "https://www.scorespro.com" (dom-attr (dom-by-tag d 'a) 'href))))))
         url-list))))
 
-(defgroup soccer-leagues nil "Customization group for soccer leagues.")
+(defgroup soccer-leagues nil
+  "Customization group for soccer leagues."
+  :group 'soccer)
 
-(defcustom soccer-leagues--leagues-alist
-  `(("England: Premier League" . ,(soccer-leagues--get-club-names-and-urls "England" "Premier League"))
-    ("Spain: LaLiga" . ,(soccer-leagues--get-club-names-and-urls "Spain" "LaLiga"))
-    ("France: Ligue 1" . ,(soccer-leagues--get-club-names-and-urls "France" "Ligue 1"))
-    ("Italy: Serie A" . ,(soccer-leagues--get-club-names-and-urls "Italy" "Serie A"))
-    ("Germany: Bundesliga" . ,(soccer-leagues--get-club-names-and-urls "Germany" "Bundesliga"))
-    ("England: Championship" . ,(soccer-leagues--get-club-names-and-urls "England" "Championship")))
-  "Alist of soccer league data."
-  ;; Specifying the table is a little klunky.  We want one table per
-  ;; league.
-  :type '(repeat (cons :tag "League"
-                       (string :tag "League Name")
-                       (repeat :tag "Teams"
-                               (cons :tag "Team/Table"
-                                     (choice
-                                      (string :tag "Name")
-                                      (const "Table"))
-                                     (string :tag "URL")))))
+(defcustom soccer-leagues-alist '(("England" . "Premier League")
+				  ("Spain" . "Laliga")
+				  ("France" . "Ligue 1")
+				  ("Italy" . "Serie A")
+				  ("Germany" . "Bundesliga")
+				  ("England" . "Championship"))
+  "Each element is a cons-cell (CONTRY . LEAGUE)."
+  :type '(repeat (cons :tag "COUNTRY"
+		       (choice
+			(string :tag "Name")
+			(const "Table"))
+		       (string :tag "CLUB")))
   :group 'soccer-leagues)
+
+(defvar soccer-leagues--leagues-alist)
+(setq soccer-leagues--leagues-alist
+      (let* ((country-list (mapcar 'car soccer-leagues-alist))
+	     (league-list (mapcar 'cdr soccer-leagues-alist)))
+	(cl-loop for n from 0 to (1- (length country-list))
+		 collect (let* ((country (nth n country-list))
+				(league (nth n league-list))
+				(name (format "%s: %s" country league))
+				(urls (soccer-leagues--get-club-names-and-urls country league)))
+			   (cons name urls)))))
 
 (provide 'soccer-leagues)
 
 ;;; soccer-leagues.el ends here
+
